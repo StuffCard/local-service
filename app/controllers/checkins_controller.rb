39e005ee07d5f61af548b98e3dbf3c32675867da
Salim Hbeiliny <salim@railslove.com>
2013-07-12 16:23:48 +0200

    checkin create

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,10 +1,20 @@
 class CheckinsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
 
   def index
 
   end
 
   def create
+    checkin = Checkin.create(checkin_params)
+    if checkin.save
+      head :created
+    else
+      head :bad_request
+    end
+  end
 
+  def checkin_params
+    params.require(:checkin).permit(:smartcard_id, :reader_id, :location_key)
   end
 end
--- test/controllers/checkins_controller_test.rb
+++ test/controllers/checkins_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CheckinsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create checkin" do
+    assert_difference('Checkin.count') do
+      post :create, checkin: {smartcard_id: "123", reader_id: "1", location_key: "abc"}
+    end
+    assert_response :success
+  end
 end
