9fac981b693c7984362ef3dcde9d8a820d836ce4
Salim Hbeiliny <salim@railslove.com>
2013-07-15 16:41:17 +0200

    fix how config is read

--- config/initializers/local-service.rb
+++ config/initializers/local-service.rb
@@ -2,18 +2,18 @@ require 'yaml'
 
 SERVICE_CONFIG = YAML.load_file Rails.root.join('config', 'local-service.yml')
 
-LocalService::Application.config do |config|
+LocalService::Application.configure do
+  config.service = ActiveSupport::OrderedOptions.new
   config.service.type = SERVICE_CONFIG['type'].to_sym
 
   if config.service.type == :master
     # Master-Config goes here
-
   else
     # Slave-Config goes here
-    config.service.location.key  = SERVICE_CONFIG['location']['key']
-    config.service.location.name = SERVICE_CONFIG['location']['name']
-    config.service.location.slug = SERVICE_CONFIG['location']['slug']
+    config.service.location_key  = SERVICE_CONFIG['location']['key']
+    config.service.location_name = SERVICE_CONFIG['location']['name']
+    config.service.location_slug = SERVICE_CONFIG['location']['slug']
 
-    config.service.master.url      = SERVICE_CONFIG['master']['url']
+    config.service.master_url      = SERVICE_CONFIG['master']['url']
   end
 end
