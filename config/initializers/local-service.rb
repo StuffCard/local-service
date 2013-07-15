dfbd202c32f46c3ed3e9b10a30df3b88751f829e
Salim Hbeiliny <salim@railslove.com>
2013-07-15 17:27:31 +0200

    check if config file exists before trying to read it

--- config/initializers/local-service.rb
+++ config/initializers/local-service.rb
@@ -1,19 +1,23 @@
 require 'yaml'
 
-SERVICE_CONFIG = YAML.load_file Rails.root.join('config', 'local-service.yml')
+config_file = Rails.root.join('config', 'local-service.yml')
 
-LocalService::Application.configure do
-  config.service = ActiveSupport::OrderedOptions.new
-  config.service.type = SERVICE_CONFIG['type'].to_sym
+if File.exists? config_file
+  SERVICE_CONFIG = YAML.load_file config_file
 
-  if config.service.type == :master
-    # Master-Config goes here
-  else
-    # Slave-Config goes here
-    config.service.location_key  = SERVICE_CONFIG['location']['key']
-    config.service.location_name = SERVICE_CONFIG['location']['name']
-    config.service.location_slug = SERVICE_CONFIG['location']['slug']
+  LocalService::Application.configure do
+    config.service = ActiveSupport::OrderedOptions.new
+    config.service.type = SERVICE_CONFIG['type'].to_sym
 
-    config.service.master_url      = SERVICE_CONFIG['master']['url']
+    if config.service.type == :master
+      # Master-Config goes here
+    else
+      # Slave-Config goes here
+      config.service.location_key  = SERVICE_CONFIG['location']['key']
+      config.service.location_name = SERVICE_CONFIG['location']['name']
+      config.service.location_slug = SERVICE_CONFIG['location']['slug']
+
+      config.service.master_url      = SERVICE_CONFIG['master']['url']
+    end
   end
 end
